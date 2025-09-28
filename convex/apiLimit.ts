fcc0ff7b6a2907ba511ee560f0e0d5ba27315586
Shubham Midgule <shubham.midgule17@gmail.com>
2025-09-28 11:32:44 +0530

    Add user credits retrieval to AppSidebar and update credits display logic

--- convex/apiLimit.ts
+++ convex/apiLimit.ts
@@ -1,5 +1,35 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
+import { FREE_TIER_CREDITS } from "@/lib/api-limits";
+
+export const getUserCredits = query({
+  args: {
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    try {
+      const apiLimits = await ctx.db
+        .query("apiLimits")
+        .withIndex("by_user", (q) => q.eq("userId", args.userId))
+        .unique();
+
+      if (!apiLimits) {
+        return {
+          credits: FREE_TIER_CREDITS,
+        };
+      }
+
+      return {
+        credits: apiLimits?.credits,
+      };
+    } catch (error) {
+      return {
+        credits: 0,
+      };
+    }
+  },
+});
+
 
 export const deductCredit = mutation({
   args: { userId: v.string(), credit: v.number() },
